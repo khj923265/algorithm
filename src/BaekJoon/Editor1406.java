f844f17c3e259333b9bbf19936bff8aa607ea91a
hyunghunkim <khj923265@naver.com>
2021-06-19 15:42:19 +0900

    6/19 알고리즘 스터디 문제 제출

---
+++ src/BaekJoon/Editor1406.java
@@ -0,0 +1,82 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Editor1406 {
+
+    public static void main(String[] args) throws IOException {
+        // 시간초과 에러 뜸 뭔가 잘못 사용한듯
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder bw = new StringBuilder();
+        String str = br.readLine(); // 첫번째 입력 문자 문자열로 변환
+        List<Character> list = new LinkedList<Character>(); // 문자열을 LinkedList로 변환
+        int value = Integer.parseInt(br.readLine()); // 반복할 숫자 입력
+
+        for(int j = 0; j < str.length(); j++){
+            list.add(str.charAt(j));
+        }
+
+        int i = list.size(); // 커서의 숫자
+        // 첫번째 입력수 만큼 반복
+        while (value --> 0){
+            // !!!!시간초과!!!!
+            String editorInput = br.readLine();
+            char editor = editorInput.charAt(0);
+            switch (editor){
+                case 'L':
+                    if (i != 0){
+                        i -= 1;
+                    }
+                    break;
+                case 'D':
+                    if (i < list.size()){
+                        i += 1;
+                    }
+                    break;
+                case 'B':
+                    if (i != 0){
+                        list.remove(i-1);
+                        i -= 1;
+                    }
+                    break;
+                case 'P':
+                    char chars = editorInput.charAt(2);
+                    list.add(i,chars);
+                    i += 1;
+                    break;
+            }
+            // !!!!시간초과!!!!
+//            if(editor.contains("L")){
+//                if (i != 0){
+//                    i -= 1;
+//                }
+//            }else if (editor.contains("D")){
+//                if (i < list.size()){
+//                    i += 1;
+//                }
+//            }else if (editor.contains("B")){
+//                if (i != 0){
+//                    list.remove(i-1);
+//                    i -= 1;
+//                }
+//            }else if (editor.contains("P")){
+//                String[] strings = editor.split(" ");
+//                list.add(i,strings[1]);
+//                i += 1;
+//            }
+        }
+        for (char a : list){
+            bw.append(a);
+        }
+        System.out.println(bw);
+    }
+
+}
---
+++ src/yogiyo/CompanyEmail.java
@@ -0,0 +1,21 @@
+package yogiyo;
+
+
+public class CompanyEmail {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public String solution(String S, String C){
+        String[] arr = S.split(" ");
+
+        if (arr.length == 2){
+
+        }else {
+
+        }
+        return "";
+    }
+}
