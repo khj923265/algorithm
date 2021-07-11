a6df3cf88036c3ef8505160eda87d76a2c1f6d8c
hyunghunkim <khj923265@naver.com>
2021-07-11 16:54:33 +0900

    Card11052

--- src/BaekJoon/Card11052.java
+++ src/BaekJoon/Card11052.java
@@ -3,7 +3,7 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.stream.Stream;
+import java.util.Scanner;
 
 public class Card11052 {
 
@@ -11,16 +11,22 @@ public class Card11052 {
         solution();
     }
 
-//    public static void solution() throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringBuilder sb = new StringBuilder();
-//        int N = Integer.parseInt(br.readLine());
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(br.readLine());
 //        int[] P = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-//        int[] dp = new int[N + 1];
-//        for (int i = 0; i < N; i++){
-//            P[0] + P[N - 1]
-//            int max = Math.max(P[i]/(i + 1), P[i + 1]/(i + 2));
-//        }
-//    }
+        int[] dp = new int[N + 1];
+        int[] P = new int[N + 1];
+        for (int i = 1; i < N + 1; i++) {
+            P[i] = sc.nextInt();
+        }
 
+        for(int i = 1; i < N + 1; i++) {
+            for (int j = 1; j <= i; j++){
+                dp[i] = Math.max(dp[i],  dp[i - j] + P[j]);
+            }
+        }
+        System.out.println(dp[N]);
+    }
 }
