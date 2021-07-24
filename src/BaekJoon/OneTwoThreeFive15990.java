7575aee0af77a3cdfae9b95ed8fba03c8c08ed0e
hyunghunkim <khj923265@naver.com>
2021-07-24 18:18:57 +0900

    123plus5 완료

--- src/BaekJoon/OneTwoThreeFive15990.java
+++ src/BaekJoon/OneTwoThreeFive15990.java
@@ -6,27 +6,38 @@ import java.io.InputStreamReader;
 
 public class OneTwoThreeFive15990 {
 
-    static int[][] dp = new int[10001][4];
-    static int MOD = 1000000009;
+    static int[][] dp = new int[100001][4];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int testCase = Integer.parseInt(br.readLine());
         dp[1][1] = 1;
         dp[2][2] = 1;
         dp[3][1] = 1;
         dp[3][2] = 1;
         dp[3][3] = 1;
 
-        for (int i = 1; i < 100000; i++){
-
+        for(int i = 4; i<= 100000; i++) {
+            dp[i][1] = dp[i - 1][2] + dp[i - 1][3];
+            dp[i][2] = dp[i - 2][1] + dp[i - 2][3];
+            dp[i][3] = dp[i - 3][1] + dp[i - 3][2];
+            dp[i][1] %= 1000000009;
+            dp[i][2] %= 1000000009;
+            dp[i][3] %= 1000000009;
         }
 
+        int testCase = Integer.parseInt(br.readLine());
         while (testCase --> 0){
-
+            int n = Integer.parseInt(br.readLine());
+            int result = 0;
+            for (int i = 1; i <= 3; i++){
+                result += dp[n][i];
+                result %= 1000000009;
+            }
+            sb.append(result).append("\n");
         }
+        br.close();
         System.out.println(sb);
     }
 }
