e08dbf0153b558a63c33abaaaa0a1007da3021d8
khj923265 <khj923265@gmail.com>
2021-07-29 12:26:53 +0900

    pinary P 변경

--- src/BaekJoon/dynamicprogramming/PinaryNumber2193.java
+++ src/BaekJoon/dynamicprogramming/PinaryNumber2193.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class pinaryNumber2193 {
+public class PinaryNumber2193 {
     static long[][] dp = new long[91][2];
 
     public static void main(String[] args) throws IOException {
