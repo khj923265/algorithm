5058b3d4898727d38cc4135bbaf7a10dcd278144
khj923265 <khj923265@naver.com>
2021-08-19 17:36:24 +0900

    123더하기3 문제 중간 커밋

---
+++ src/BaekJoon/dynamicprogramming/OneTwoThree15988.java
@@ -0,0 +1,27 @@
+package BaekJoon.dynamicprogramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class OneTwoThree15988 {
+    private static final int maxIndex = 1_000_000;
+    private static final int MOD = 1_000_000_009;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        int[] dp = new int[maxIndex + 1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        while (T --> 0){
+
+        }
+
+
+
+    }
+}
