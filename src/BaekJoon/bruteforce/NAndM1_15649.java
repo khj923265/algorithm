82f2edc4fc1c50fa2c22a024a3b33c34e9eb55ff
hyunghunkim <khj923265@naver.com>
2021-10-28 22:04:58 +0900

    NAndM1 재귀로 해결해야함

--- src/BaekJoon/bruteforce/NAndM1_15649.java
+++ src/BaekJoon/bruteforce/NAndM1_15649.java
@@ -3,17 +3,26 @@ package BaekJoon.bruteforce;
 public class NAndM1_15649 {
 
     public static void main(String[] args) throws Exception {
+        /**
+         *  N 과 M (1) 문제
+         *  1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열 출력하기.
+         */
+
+        // ex) 4 2
+        //     1 2, 1 3, 1 4, 2 1, 2 3 ...
+
+        //TODO 재귀로 해결
         StringBuilder sb = new StringBuilder();
         int N = read();
         int M = read();
 
         for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= M; j++) {
-                if (i == j) break;
-                sb.append(i).append(" ").append(j).append("\n");
+            for (int j = 1; j <= N; j++) {
+                if (i != j) {
+
+                }
             }
         }
-
 
         System.out.println(sb);
 
