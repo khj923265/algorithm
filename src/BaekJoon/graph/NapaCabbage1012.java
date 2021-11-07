f7cab7ea9d88b5deae57f6dc6182542a805023ba
hyunghunkim <khj923265@naver.com>
2021-11-07 15:01:56 +0900

    바이러스 문제 해결

---
+++ src/BaekJoon/graph/NapaCabbage1012.java
@@ -0,0 +1,72 @@
+package BaekJoon.graph;
+
+public class NapaCabbage1012 {
+    private static int M;
+    private static int N;
+    private static int K;
+    private static int earthwormNumber;
+    private static int[][] list;
+    private static boolean[][] check;
+
+    public static void main(String[] args) throws Exception {
+        /**
+         *  유기농 배추 문제
+         *  DFS 와 BFS 문제
+         */
+
+        int t = read();
+        StringBuilder sb = new StringBuilder();
+
+        while (t--> 0) {
+            M = read();
+            N = read();
+            K = read();
+
+            list = new int[N][M];
+            check = new boolean[N][M];
+            earthwormNumber = 0;
+
+            for (int i = 0; i < K; i++) {
+                int x = read();
+                int y = read();
+
+                list[y][x] = 1;
+            }
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (!check[i][j] && list[i][j] == 1) {
+                        dfs(i , j);
+                        ++earthwormNumber;
+                    }
+                }
+            }
+            sb.append(earthwormNumber).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void dfs(int n, int m) {
+        if (n < N && m < M) {
+            if (!check[n][m] && list[n][m] == 1) {
+                dfs(n,m + 1);
+                dfs(n + 1, m);
+            }
+            check[n][m] = true;
+        }
+    }
+
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean isNegative = n == 13;
+        if (isNegative) {
+            n = System.in.read() & 15;
+        }
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return isNegative ? ~n + 1 : n;
+    }
+
+}
---
+++ src/BaekJoon/graph/Virus2606.java
@@ -0,0 +1,57 @@
+package BaekJoon.graph;
+
+public class Virus2606 {
+    private static int comCount;
+    private static int infectionCount;
+    private static int[][] list;
+    private static boolean[] check;
+
+    public static void main(String[] args) throws Exception {
+        comCount = read();
+        int testCase = read();
+
+        infectionCount = 0;
+        list = new int[comCount + 1][comCount + 1];
+        check = new boolean[comCount + 1];
+
+        for (int i = 1; i <= testCase; i++) {
+            int x = read();
+            int y = read();
+
+            list[x][y] = 1;
+            list[y][x] = 1;
+        }
+
+        dfs(1);
+
+        System.out.println(infectionCount);
+
+
+    }
+
+    private static void dfs(int comNumber) {
+        if (check[comNumber]) {
+            return;
+        }
+
+        check[comNumber] = true;
+        for (int i = 1; i <= comCount; i++) {
+            if (!check[i] && list[comNumber][i] == 1) {
+                dfs(i);
+                ++infectionCount;
+            }
+        }
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean isNegative = n == 13;
+        if (isNegative) {
+            n = System.in.read() & 15;
+        }
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return isNegative ? ~n + 1 : n;
+    }
+}
