136243e2b9b39294df55c5c6c07fe47cd41b732d
hyungjun <khj923265@naver.com>
2021-12-12 13:59:28 +0900

    소수찾기 문제 해결 전

---
+++ src/BaekJoon/class2/PrimeNumber1978.java
@@ -0,0 +1,14 @@
+package BaekJoon.class2;
+
+public class PrimeNumber1978 {
+
+    public static void main(String[] args) {
+        /**
+         * 소수 찾기 문제
+         */
+
+        // 1과 자기자신을 제외 후 2, 3, 5, 7 의 배수에 해당하는 값은 삭제
+
+    }
+
+}
