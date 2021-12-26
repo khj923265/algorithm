8a7f5d8cb8d6e8c3985b7e16b6610849e94f6e11
hyungjun <khj923265@naver.com>
2021-12-26 13:29:26 +0900

    숨바꼭질 문제 해결

--- src/BaekJoon/graph/HideAndSeek1697.java
+++ src/BaekJoon/graph/HideAndSeek1697.java
@@ -5,6 +5,7 @@ import java.util.Queue;
 
 public class HideAndSeek1697 {
     private static Queue<Integer> queue;
+    private static int[] visit;
     private static int brotherLocation;
     private static int currentLocation;
     private static int count;
@@ -16,38 +17,49 @@ public class HideAndSeek1697 {
 
         // hint 수빈이가 5-10-9-18-17 순으로 가면 4초만에 동생을 찾을 수 있다.
 
-        // 1. 현재위치*2 가 동생위치보다 2이상 크다면 -1
-        // 2. 현재위치*2 가 동생위치보다 2이상 작다면 *2
-        // 3. +1
-
-        // 4 17 일 경우 16일때 +1이 아닌 -1이 실행되는 문제가 있음
-
-        // 접근 잘못함 bfs 로 해결해야하는것 같은데?
-
         queue = new LinkedList<>();
+        visit = new int[100_001];
         count = 0;
 
         currentLocation = read();
         brotherLocation = read();
 
-        while (brotherLocation != currentLocation) {
-            if (brotherLocation + 1 < currentLocation * 2) {
-                currentLocation -= 1;
-                count++;
-            } else if (currentLocation * 2 <= brotherLocation + 1 || currentLocation * 2 <= brotherLocation - 1) {
-                currentLocation *= 2;
-                count++;
-            } else {
-                currentLocation++;
-                count++;
-            }
+        if (currentLocation == brotherLocation) {
+            System.out.println(0);
+        } else {
+            bfs();
         }
-
-        System.out.println(count);
     }
 
     private static void bfs() {
-        queue.offer(0);
+        queue.offer(currentLocation);
+        visit[currentLocation] = 1;
+
+        while (!queue.isEmpty()) {
+            int location = queue.poll();
+
+            for (int i = 0; i < 3; i++) {
+                int next;
+
+                if (i == 0) {
+                    next = location * 2;
+                } else if (i == 1) {
+                    next = location + 1;
+                } else {
+                    next = location - 1;
+                }
+
+                if (next == brotherLocation) {
+                    System.out.println(visit[location]);
+                    return;
+                }
+
+                if (0 <= next && next < 100_001 && visit[next] == 0) {
+                    queue.offer(next);
+                    visit[next] = visit[location] + 1;
+                }
+            }
+        }
     }
 
     private static int read() throws Exception {
