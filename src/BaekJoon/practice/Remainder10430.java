61250c71aa0002bb5ae2719417a2138656210773
khj923265 <khj923265@naver.com>
2022-01-03 12:41:28 +0900

    나머지 문제 해결

---
+++ src/BaekJoon/practice/Remainder10430.java
@@ -0,0 +1,34 @@
+package BaekJoon.practice;
+
+public class Remainder10430 {
+
+    public static void main(String[] args) throws Exception {
+        /**
+         * 나머지 문제
+         */
+
+        // 첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+
+        StringBuilder sb =  new StringBuilder();
+
+        int A = read();
+        int B = read();
+        int C = read();
+
+        sb.append((A+B)%C).append("\n");
+        sb.append(((A%C) + (B%C))%C).append("\n");
+        sb.append((A*B)%C).append("\n");
+        sb.append(((A%C) * (B%C))%C).append("\n");
+
+        System.out.println(sb);
+
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean isNegative = n == 13;
+        if (isNegative) n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return isNegative ? ~n + 1 : n;
+    }
+}
