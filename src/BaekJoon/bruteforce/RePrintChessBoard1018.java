4d01fb8fa0672962cbd192f080ff5bf9b338101f
= <khj923265@naver.com>
2022-01-06 12:58:41 +0900

    체스판 다시칠하기 문제 해결중

--- src/BaekJoon/bruteforce/RePrintChessBoard1018.java
+++ src/BaekJoon/bruteforce/RePrintChessBoard1018.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class RePrintChessBoard1018 {
-    private static String[][] boards;
+    private static String[] boards;
 
     public static void main(String[] args) throws IOException {
         /**
@@ -16,10 +16,16 @@ public class RePrintChessBoard1018 {
         String[] boardSize = br.readLine().split(" ");
         int N = Integer.parseInt(boardSize[0]);
         int M = Integer.parseInt(boardSize[1]);
-        boards = new String[N][M];
+        boards = new String[N];
 
         for (int i = 0; i < N; i++) {
-            boards[i] = br.readLine().split("");
+            boards[i] = br.readLine();
+        }
+
+        for (int i = 0; i < N - 8; i++) {
+            for (int j = 0; j < M - 1; j++) {
+
+            }
         }
 
     }
@@ -27,8 +33,12 @@ public class RePrintChessBoard1018 {
     private static void CountRePrintBoard(int x, int y) {
         for (int i = x; i < x + 8; i++) {
             for (int j = y; j < y + 8; j++) {
-
+                test(boards[i].charAt(j));
             }
         }
+    }
+
+    private static void test(char a) {
+
     }
 }
