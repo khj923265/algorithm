6548178c34172665a5c9128ee827bb7252f177ad
= <khj923265@naver.com>
2022-05-19 15:41:24 +0900

    Feat: 영역 구하기 문제 해결

--- src/jungol/graph/FindArea1457.java
+++ src/jungol/graph/FindArea1457.java
@@ -8,7 +8,12 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class FindArea1457 {
-    private static int[][] list;
+    private static boolean[][] board;
+    private static final int[] dx = { -1, 0, 1, 0 };
+    private static final int[] dy = { 0, 1, 0, -1 };
+    private static int maxXIndex;
+    private static int maxYIndex;
+    private static int count;
 
     public static void main(String[] args) throws IOException {
         /**
@@ -18,21 +23,56 @@ public class FindArea1457 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine(),  " ");
-        int x = Integer.parseInt(st.nextToken());
         int y = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
         int rectangleCnt = Integer.parseInt(st.nextToken());
 
-        list = new int[x][y];
+        board = new boolean[x][y];
+        maxXIndex = x;
+        maxYIndex = y;
         List<Rectangle> rectangles = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
 
         for (int i = 0; i < rectangleCnt; i++) {
             String[] coordinates = br.readLine().split(" ");
             rectangles.add(new Rectangle(coordinates));
         }
 
-        for (Rectangle rectangle : rectangles) {
+        rectangles.forEach(Rectangle::checked);
 
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                if (!board[i][j]) {
+                    count = 0;
+                    dfs(i,j);
+                    result.add(count);
+                }
+            }
         }
+
+        result.sort(Integer::compareTo);
+        System.out.println(result.size());
+        result.forEach(count -> System.out.print(count + " "));
+
+    }
+
+    public static void dfs(int x, int y) {
+        board[x][y] = true;
+
+        count = count + 1;
+
+        for (int i = 0; i < 4; i++) {
+            int nextX = dx[i] + x;
+            int nextY = dy[i] + y;
+            if (nextX < 0 || nextY < 0 || maxXIndex <= nextX || maxYIndex <= nextY) {
+                continue;
+            }
+            if (board[nextX][nextY]) {
+                continue;
+            }
+            dfs(nextX, nextY);
+        }
+
     }
 
     private static class Rectangle{
@@ -48,36 +88,12 @@ public class FindArea1457 {
             this.endY = Integer.parseInt(coordinates[3]);
         }
 
-        public int getStartX() {
-            return startX;
-        }
-
-        public void setStartX(int startX) {
-            this.startX = startX;
-        }
-
-        public int getStartY() {
-            return startY;
-        }
-
-        public void setStartY(int startY) {
-            this.startY = startY;
-        }
-
-        public int getEndX() {
-            return endX;
-        }
-
-        public void setEndX(int endX) {
-            this.endX = endX;
-        }
-
-        public int getEndY() {
-            return endY;
-        }
-
-        public void setEndY(int endY) {
-            this.endY = endY;
+        public void checked() {
+            for (int i = startX; i < endX; i++) {
+                for (int j = startY; j < endY; j++) {
+                    board[i][j] = true;
+                }
+            }
         }
     }
 }
