233962f456500e3581670c47c4a7317ec6ddc638
khj923265 <khj923265@naver.com>
2022-07-29 13:35:07 +0900

    Z 문제 해결

--- src/BaekJoon/class3/Z1074.java
+++ src/BaekJoon/class3/Z1074.java
@@ -3,54 +3,45 @@ package BaekJoon.class3;
 public class Z1074 {
     /**
      * Z 문제
+     * 분할정복 문제
      */
 
-    private static int[] dx = {0, 0, 1, 1};
-    private static int[] dy = {0, 1, 0, 1};
-    private static int row;
-    private static int col;
-
     private static int count = 0;
-    private static int[][] list;
-
     public static void main(String[] args) throws Exception {
         int N = read();
-        row = read();
-        col = read();
-        int index = (int) Math.pow(2, N);
-        list = new int[index][index];
-        z(0, 0, index - 1, index - 1);
+        int row = read();
+        int col = read();
+        int size = (int) Math.pow(2, N);
+        findZCount(size, row, col);
+        System.out.println(count);
     }
 
-    //TODO list 쓰지말고 그냥 값으로만 나누면서 사분면을 계속 쪼개서 찾아야함
-    private static void z(int startX, int startY, int endX, int endY) {
-//        if (row < startX || col < startY
-//                || endX < row || endY < col) {
-//            int x = Math.abs(startX - endX) + 1;
-//            int y = Math.abs(startY - endY) + 1;
-//            count = count + (x * y);
-//            return;
-//        }
-        if (Math.abs(startX - endX) == 1) {
-            countZ(startX, startY);
+    private static void findZCount(int size, int r, int c) {
+        if (size == 1) {
             return;
         }
-        z(startX, startY, (endX) / 2, (endY) / 2);
-        z(startX, (endY + 1) / 2, (endX) / 2, endY);
-        z((endX + 1) / 2, startY, endX, (endY) / 2);
-        z((endX + 1) / 2, (endY + 1) / 2, endX, endY);
-    }
-
-    private static void countZ(int startX, int startY) {
-        for (int i = 0; i < 4; i++) {
-            int sX = startX + dx[i];
-            int sY = startY + dy[i];
-            if (sX == row && sY == col) {
-                System.out.println(count);
-                System.exit(0);
-            }
-            count = count + 1;
+        int halfSize = size/2;
+        
+        // 1사분면에 있을 경우
+        if (r < halfSize && c < halfSize) {
+            findZCount(halfSize, r, c);
+            return;
         }
+        // 2사분면에 있을 경우
+        if (r < halfSize) {
+            count = count + (halfSize * halfSize);
+            findZCount(halfSize, r, c - halfSize);
+            return;
+        }
+        // 3사분면에 있을 경우
+        if (c < halfSize) {
+            count = count + (halfSize * halfSize) * 2;
+            findZCount(halfSize, r - halfSize, c);
+            return;
+        }
+        // 4사분면에 있을 경우
+        count = count + (halfSize * halfSize) * 3;
+        findZCount(halfSize, r - halfSize, c - halfSize);
     }
 
     private static int read() throws Exception {
